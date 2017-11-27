404e45e3545a3053bde0cc062a39bdce71d26b69
Alex Fruchtman <sasha.kis@gmail.com>
2017-11-27 15:26:04 +0200

    Add login html panel and user constractors

---
+++ app/model/user.js
@@ -0,0 +1,13 @@
+// Shared User Constructor
+recipeApp.factory("User", function(){
+    function User(plainObject) {
+        this.email = plainObject.email;
+        this.password = plainObject.password;
+        this.firstName = plainObject.firstName;
+        this.lastName = plainObject.lastName;
+        this.role = plainObject.role;
+        this.data = plainObject.data;
+    };
+
+    return User;
+});
