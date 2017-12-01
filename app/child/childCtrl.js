6ee641dcc2aa55229697f4a1e0ad1148d684a6b6
Alex Fruchtman <sasha.kis@gmail.com>
2017-12-01 17:18:21 +0200

    Two different silders on child page

--- app/child/childCtrl.js
+++ app/child/childCtrl.js
@@ -1,5 +1,5 @@
 bestCoinApp.controller("ChildCtrl", function ($scope, $http, $location, $log, activeUser, Report, children) {
-     $scope.test = "test data";
+    $scope.test = "test data";
     //$scope.mood = "5"
     //http call to get data from json
     if (!activeUser.isLoggedIn()) {
@@ -17,24 +17,29 @@ bestCoinApp.controller("ChildCtrl", function ($scope, $http, $location, $log, ac
         children.loadAll(response.data);
         //$log.log(children.getAll());
         $scope.childArr = children.getAll();
-        $scope.childMood = $scope.childArr[$scope.childArr.length-1].mood;
+        $scope.childMood = $scope.childArr[$scope.childArr.length - 1].mood;
         $log.log($scope.childMood);
-        $scope.subjects = $scope.childArr[$scope.childArr.length-1].subjects;
+        $scope.subjects = $scope.childArr[$scope.childArr.length - 1].subjects;
         $log.log($scope.subjects);
     });
 
-    $scope.slider = document.getElementById("myMood");
-    $scope.output = document.getElementById("childmood");
-    $scope.output.innerHTML = $scope.slider.value;
-    //$log.log($scope.slider.value)
+    $scope.sliderMood = document.getElementById("myMood");
+    $scope.outputMood = document.getElementById("childmood");
+    $scope.outputMood.innerHTML = $scope.sliderMood.value;
+     $scope.sliderMood.oninput = function () {
+         $scope.outputMood.innerHTML = this.value;
+     }
 
+    $scope.slider = document.getElementById("myhealth");
+    $scope.output = document.getElementById("childhealth");
+    $scope.output.innerHTML = $scope.slider.value;
     $scope.slider.oninput = function () {
         $scope.output.innerHTML = this.value;
     }
 
 
     // var cuisines = ["Chinese","Indian"];     
-    
+
     // var sel = document.getElementById('CuisineList');
     // for(var i = 0; i < cuisines.length; i++) {
     //     var opt = document.createElement('option');
@@ -42,5 +47,5 @@ bestCoinApp.controller("ChildCtrl", function ($scope, $http, $location, $log, ac
     //     opt.value = cuisines[i];
     //     sel.appendChild(opt);
     // }
-    
+
 });
